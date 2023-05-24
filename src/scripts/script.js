001633dfc7534c220b5afdfd0910a5b627e9d079
Nikita Serov <stud0000262775@study.utmn.ru>
2023-05-24 23:11:40 +0500

    initial commit

---
+++ src/scripts/script.js
@@ -0,0 +1,57 @@
+import $ from 'jquery';
+$(document).ready(function() {
+    $('#openModal').click(function() {
+        $('#modalBackground').show();
+        $('#modalWindow').show();
+    });
+
+    $('#closeModal').click(function() {
+        $('#modalBackground').hide();
+        $('#modalWindow').hide();
+    });
+});
+
+
+//////////////////////////////////
+
+
+
+$(document).ready(function() {
+    var slideWidth = $('.slider').width();
+    var totalSlides = $('.slides-container img').length;
+    var currentSlide = 0;
+
+    $('.slides-container').css('width', slideWidth * totalSlides);
+
+    function updateButtons() {
+        if (currentSlide === 0) {
+            $('.prev-button').addClass('button-disabled');
+        } else {
+            $('.prev-button').removeClass('button-disabled');
+        }
+
+        if (currentSlide === totalSlides - 1) {
+            $('.next-button').addClass('button-disabled');
+        } else {
+            $('.next-button').removeClass('button-disabled');
+        }
+    }
+
+    $('.prev-button').click(function() {
+        if (currentSlide > 0) {
+            currentSlide--;
+            $('.slides-container').css('transform', 'translateX(-' + (currentSlide * slideWidth) + 'px)');
+            updateButtons();
+        }
+    });
+
+    $('.next-button').click(function() {
+        if (currentSlide < totalSlides - 1) {
+            currentSlide++;
+            $('.slides-container').css('transform', 'translateX(-' + (currentSlide * slideWidth) + 'px)');
+            updateButtons();
+        }
+    });
+
+    updateButtons();
+});
